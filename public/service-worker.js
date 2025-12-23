068d77cae742c55a7655a3636f7208976615b7cf
Ahmed-Sallam22 <ahmed30sallam22@gmail.com>
2025-12-23 08:45:06 +0200

    handel cashe

---
+++ public/service-worker.js
@@ -0,0 +1,61 @@
+// Service Worker for Cache Management
+// This will help clear browser cache on new deployments
+
+const CACHE_NAME = 'tanfeez-v' + new Date().getTime();
+const urlsToCache = [
+  '/',
+  '/index.html'
+];
+
+// Install event - cache resources
+self.addEventListener('install', (event) => {
+  // Skip waiting to activate immediately
+  self.skipWaiting();
+  
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then((cache) => cache.addAll(urlsToCache))
+  );
+});
+
+// Activate event - clean up old caches
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((cacheNames) => {
+      return Promise.all(
+        cacheNames.map((cacheName) => {
+          if (cacheName !== CACHE_NAME) {
+            console.log('Deleting old cache:', cacheName);
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
+  );
+  
+  // Take control of all clients immediately
+  return self.clients.claim();
+});
+
+// Fetch event - network first strategy
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    fetch(event.request)
+      .then((response) => {
+        // Clone the response
+        const responseToCache = response.clone();
+        
+        // Cache the new response
+        caches.open(CACHE_NAME)
+          .then((cache) => {
+            cache.put(event.request, responseToCache);
+          });
+        
+        return response;
+      })
+      .catch(() => {
+        // If network fails, try cache
+        return caches.match(event.request);
+      })
+  );
+});
